9c922862924bf31384213a608d3c5f25c7093739
Nick Hofstede <nick.hofstede@gmail.com>
2025-12-05 22:33:39 +0100

    Day 5

---
+++ day5/day5.py
@@ -0,0 +1,28 @@
+part1 = 0
+ranges = []
+with open("day5/sample.txt", "r") as file:
+    readingRanges = True
+    for line in file:
+        line = line.strip()
+        if readingRanges:
+            if len(line) == 0:
+                readingRanges = False
+                continue
+            [begin, end] = line.split("-")
+            ranges.append(range(int(begin), int(end) + 1))
+        else:
+            if next((True for r in ranges if int(line) in r), False):
+                part1 += 1
+print("part 1: ", part1)
+
+part2 = 0
+index = 0
+ranges.sort(key=lambda r: r.start)
+for r in ranges:
+    if r.start >= index:
+        part2 += len(r)
+        index = r.stop
+    elif r.start < index and index < r.stop:
+        part2 += len(r) - index + r.start
+        index = r.stop
+print("part 2:", part2)
